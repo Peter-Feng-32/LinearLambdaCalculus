cedef6a3e256193cb0b82cfed752d3f811318f86
Vedaant Shah <svedaant@gmail.com>
2024-03-30 16:58:48 -0400

    Add support for product types.

--- base.py
+++ base.py
@@ -31,11 +31,20 @@ class Application(Expression):
         self.argument = argument   
     def __str__(self) -> str:
         return f"(({self.function}) ({self.argument}))"
+
+class Product(Expression):
+    # Product type is not part of default STLC, it is something we need to add in
+    def __init__(self, e1 : Expression, e2: Expression) -> None:
+        self.e1 = e1
+        self.e2 = e2
+    def __str__(self) -> str:
+        return f"<{self.e1}, {self.e2}>"
     
 class Type:
-    def __init__(self, is_unit, t1=None, t2=None):
+    def __init__(self, is_unit=False, is_product=False, t1=None, t2=None):
         self.is_unit = is_unit
+        self.is_product = is_product
         self.t1 = t1
         self.t2 = t2
     def __eq__(self, other):
-        return self.is_unit == other.is_unit and self.t1 == other.t1 and self.t2 == other.t2
+        return self.is_unit == other.is_unit and self.is_product == other.is_product and self.t1 == other.t1 and self.t2 == other.t2
--- type_check.py
+++ type_check.py
@@ -1,4 +1,4 @@
-from base import Type, Expression, Unit, Variable, Abstraction, Application
+from base import Type, Expression, Product, Unit, Variable, Abstraction, Application
 
 def partition_context(context):
     lst = [(k, v) for k, v in context.items()]
@@ -9,13 +9,15 @@ def partition_context(context):
         context1, context2 = {}, {}
         for index in range(n):
             if i & (1 << index):
-                context1[lst[index][0]] = lst[index][2]
+                context1[lst[index][0]] = lst[index][1]
             else:
-                context2[lst[index][0]] = lst[index][2]
+                context2[lst[index][0]] = lst[index][1]
         contexts.append((context1, context2))
+    
+    return contexts
 
 
-def check_with_context(expression, type, context):
+def check_with_context(expression, type, context={}):
     if isinstance(expression, Unit):
         return type.is_unit and not context
     elif isinstance(expression, Variable):
@@ -24,19 +26,30 @@ def check_with_context(expression, type, context):
         context[expression.param] = type.t1
         return check_with_context(expression.body, type.t2, context)
     elif isinstance(expression, Application):
+        if not context:
+            return check_with_context(expression.function, type.t1, context1) and check_with_context(expression.argument, type.t2, context2)
         partitions = partition_context(context)
         for context1, context2 in partitions:
             if check_with_context(expression.function, type.t1, context1) and check_with_context(expression.argument, type.t2, context2):
                 return True
         return False
-
-def check(expression, type):
-    return check_with_context(expression, type, {})
+    elif isinstance(expression, Product):
+        if not type.is_product:
+            return False
+        elif not context:
+            return check_with_context(expression.e1, type.t1, context1) and check_with_context(expression.e2, type.t2, context2)
+        partitions = partition_context(context)
+        for context1, context2 in partitions:
+            if check_with_context(expression.e1, type.t1, context1) and check_with_context(expression.e2, type.t2, context2):
+                return True
+        return False
 
 # Testing
 e1, t1 = Unit(), Type(True) # passes
-e2, t2 = Abstraction(Variable("x"), Unit()), Type(False, Type(True), Type(True)) # fails because we do not use the fact that x is of the Unit Type (as desired)
-e3, t3 = Abstraction(Variable("x"), Variable("x")), Type(False, Type(True), Type(True)) # passes as we use x 
-print(check(e1, t1))
-print(check(e2, t2))
-print(check(e3, t3))
+e2, t2 = Abstraction(Variable("x"), Unit()), Type(False, t1=Type(True), t2=Type(True)) # fails because we do not use the fact that x is of the Unit Type (as desired)
+e3, t3 = Abstraction(Variable("x"), Variable("x")), Type(False, t1=Type(True), t2=Type(True)) # passes as we use x
+e4, t4 = Abstraction(Variable("x"), Product(Variable("x"), Variable("x"))), Type(False, t1=Type(True), t2=Type(False, True, t1=Type(True), t2=Type(True))) # fails because we use x twice
+print(check_with_context(e1, t1))
+print(check_with_context(e2, t2))
+print(check_with_context(e3, t3))
+print(check_with_context(e4, t4))
